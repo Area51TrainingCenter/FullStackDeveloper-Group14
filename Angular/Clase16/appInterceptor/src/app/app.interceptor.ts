c5170ff92cbb74f46ed3badebf06e471f3544696
Eduardo Hoyos <edu.hoyos92@gmail.com>
2019-09-24 20:06:36 -0500

    Interceptor

--- Angular/Clase16/appInterceptor/src/app/app.interceptor.ts
+++ Angular/Clase16/appInterceptor/src/app/app.interceptor.ts
@@ -6,9 +6,15 @@ export class AppInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
-    let peticionClonada: HttpRequest<any> = req.clone({ headers: req.headers.append("Authorization", "bearer " + token) })
+    let peticionClonada
+
+    if (req.url.toString().indexOf("/alumno") > -1) {
+      peticionClonada = req.clone({ headers: req.headers.append("Authorization", "bearer " + token) })
+    }
 
     console.log("petición clonada")
+
+    console.log(req)
 
     return next.handle(peticionClonada)
   }
